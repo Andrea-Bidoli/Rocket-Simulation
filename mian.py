34337df830b24ab2523085bed63514c5978b6957
Andrea Bidoli <129420870+Andrea-Bidoli@users.noreply.github.com>
2024-05-16 19:35:28 +0200

    modified:   mian.py

--- mian.py
+++ mian.py
@@ -5,14 +5,21 @@ class Rocket:
     fuel_mass = 0
     fuel_burn_rate = 0
     fuel_burn_time = 0
+    thrust = 0
+    
     velocity = 0
+    acceleration = 0
+    x_distance = 0
+    height = 0
+    
     pitch = 0
     roll = 0
     yaw = 0
-    thrust = 0
+    
     thrust_pitch = 0
     thrust_roll = 0
     thrust_yaw = 0
+    
     height = 0
     diameter = 0
     def __init__(self, diameter, height, mass, fuel_mass, fuel_burn_rate, fuel_burn_time, pitch=0, roll=0, yaw=0):
@@ -30,3 +37,7 @@ class Rocket:
         self.thrust_roll = 0
         self.thrust_yaw = 0
         self.velocity = 0
+    
+    def update_velocity(self):
+        self.velocity += self.acceleration
+        
